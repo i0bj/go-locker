bd4f3c7842147bf98ed2a5f000a88c766dbc9003
obehi.iredia@gmail.com <obehi.iredia@gmail.com>
2020-05-13 02:30:48 -0400

    Add hash func

--- Internals/symmetric.go
+++ Internals/symmetric.go
@@ -1,8 +1,20 @@
 package symmetric
 
 import (
-    "os"
-	"path/filepath"
-	"io/ioutil"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
 )
 
+type AESCIPHER struct {
+	bs  []byte
+	key []byte
+}
+
+// Function to create 32 byte hash.
+func newHash(key []byte) []byte {
+	key := []byte("dfkjasfajsfhdjhadiawe110")
+	hasher := sha256.New()
+	hasher.Write(key)
+	fmt.Println(hex.EncodeToString(hasher.Sum(key)))
+}
