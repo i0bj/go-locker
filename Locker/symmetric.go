f72d41efccc691308ae808a92f5bbdea7f3f1d55
obehi.iredia@gmail.com <obehi.iredia@gmail.com>
2020-05-20 20:00:48 -0400

    Update directory name

---
+++ Locker/symmetric.go
@@ -0,0 +1,44 @@
+package symmetric
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"fmt"
+	"io"
+)
+
+// Generates ranndom key used to encrypt each file
+func keyGen() *[]byte {
+	key := make([]byte, 32)
+	_, err := rand.Read(key)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return &key
+}
+
+// Encrypt func encrypts files using our random key and file.
+// the result will be an AES cipher block
+func encrypt(data []byte, key []byte) *[]byte {
+	cipherBlock, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	// Galois Counter Mode being applied to AES cipher block.
+	gcm, err := cipher.NewGCM(cipherBlock)
+	if err != nil {
+		return nil, err
+	}
+	// Number used once
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, err
+	}
+
+	cipherText := gcm.Seal(nonce, nonce, data)
+	return &cipherText
+
+}
+
+func encryptFile(filepath string)
